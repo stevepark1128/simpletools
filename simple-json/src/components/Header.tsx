b1ae3c23cd1516df9b859aa707693886d9e2b747
spark1128 <stevepark1128@gmail.com>
2022-11-13 23:05:29 -0800

    additional update for init code

---
+++ simple-json/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+
+
+interface HeaderProps {
+    title?: string;
+  }
+
+const Header = ({title} : HeaderProps) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+export default Header
+
+Header.defaultProps = {
+    title: 'Simple Json Viewer',
+}
